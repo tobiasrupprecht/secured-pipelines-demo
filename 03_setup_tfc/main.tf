abddb772c11647318ddfa78f9d79a7b5aa029a01
tobiasrupprecht <trupprecht@hashicorp.com>
2022-05-18 14:41:16 +0200

    changed to different TFC org in Step 03

--- 03_setup_tfc/main.tf
+++ 03_setup_tfc/main.tf
@@ -3,12 +3,12 @@ provider "tfe" {
 }
 
 # Using an existing org as this is TFC; can be replaced with code to create an org if TFE is being used.
-data "tfe_organization" "mkaesz-dev" {
-  name = "mkaesz-dev"
+data "tfe_organization" "hashicorp-tobiasrupprecht" {
+  name = "hashicorp-tobiasrupprecht"
 }
 
 resource "local_file" "org" {
-    content  = data.tfe_organization.mkaesz-dev.name
+    content  = data.tfe_organization.hashicorp-tobiasrupprecht.name
     filename = "temp_data/tfc_org_name"
 }
 
